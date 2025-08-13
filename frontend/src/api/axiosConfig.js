6b917a75bbd6ec5bc1d708cb04ab33006e306b5c
Tarique <mdtariqueh1@gmail.com>
2025-08-13 21:20:54 +0530

    update axios baseURL to production server

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL : "http://localhost:3000"
+    baseURL : "https://moody-player-a7c1.onrender.com"
 })
 
 export default instance
